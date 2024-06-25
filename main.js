15dcd92680c9a87ea24b83de8d85aecbc946b012
Kwame Kyeremeh Addae-Mensah <addaemensahkwame@gmail.com>
2024-06-25 16:50:28 +0000

    Home page not done yet will change the image and some stuffs

--- main.js
+++ main.js
@@ -28,3 +28,12 @@ const navMenu = document.getElementById('nav-menu'),
         navMenu.classList.remove('show-menu')
       }
       navLink.forEach(n => n.addEventListener('click', linkAction))
+
+      /*==== SHADOW HEADER ==== */
+      const shadowHeader = () =>{
+        const header = document.getElementById('header')
+
+        this.scrollY >= 50 ? header.classList.add('shadow-header')
+                           : header.classList.remove('shadow-header')
+      }
+      window.addEventListener('scroll', shadowHeader)
